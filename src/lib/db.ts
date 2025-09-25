cea102b59cf3915a5c2cd8a6590445ee1909b443
CharlesPutra <charlesagustianputra@gmail.com>
2025-09-25 12:21:28 +0700

    lanjut nampilin data

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+    
+);
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+export default supabase;
